39e798abd2e894917a5df7e35abb5ed66e758dbd
Magid William <Maged.Ewilliam@gmail.com>
2026-07-18 21:22:49 +0300

    Exp06: LED blink + USB test to verify DTS fix

--- zephyr-app/src/main.c
+++ zephyr-app/src/main.c
@@ -1,13 +1,18 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/usb/usb_device.h>
+#include <zephyr/drivers/gpio.h>
 
 void main(void)
 {
+    const struct device *gpio0 = DEVICE_DT_GET(DT_NODELABEL(gpio0));
+    gpio_pin_configure(gpio0, 15, GPIO_OUTPUT_ACTIVE);
+
     usb_enable(NULL);
 
     while (1) {
+        gpio_pin_toggle(gpio0, 15);
         printk("Hello World from Zephyr!\n");
-        k_sleep(K_SECONDS(3));
+        k_sleep(K_SECONDS(1));
     }
 }
