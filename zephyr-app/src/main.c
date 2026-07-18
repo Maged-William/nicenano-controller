a1d5e5a6cd126ed7e0826a33ca382ebaa05ef5c4
Magid William <Maged.Ewilliam@gmail.com>
2026-07-18 21:26:27 +0300

    Exp06: Add USB serial number and DTR gating for reliable terminal capture

--- zephyr-app/src/main.c
+++ zephyr-app/src/main.c
@@ -2,17 +2,26 @@
 #include <zephyr/sys/printk.h>
 #include <zephyr/usb/usb_device.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/uart.h>
 
 void main(void)
 {
+    const struct device *cdc = DEVICE_DT_GET(DT_NODELABEL(cdc_acm_uart0));
     const struct device *gpio0 = DEVICE_DT_GET(DT_NODELABEL(gpio0));
+    uint32_t dtr = 0;
+
     gpio_pin_configure(gpio0, 15, GPIO_OUTPUT_ACTIVE);
 
     usb_enable(NULL);
 
+    while (!dtr) {
+        uart_line_ctrl_get(cdc, UART_LINE_CTRL_DTR, &dtr);
+        k_sleep(K_MSEC(100));
+    }
+
     while (1) {
         gpio_pin_toggle(gpio0, 15);
         printk("Hello World from Zephyr!\n");
-        k_sleep(K_SECONDS(1));
+        k_sleep(K_SECONDS(3));
     }
 }
